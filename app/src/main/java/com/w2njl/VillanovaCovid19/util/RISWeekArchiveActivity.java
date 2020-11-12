988a95839d58d361535a679eb78734f297fe6826
W2NJL <nlanga01@villanova.edu>
2020-11-12 14:13:52 -0500

    Initial commit

---
+++ app/src/main/java/com/w2njl/VillanovaCovid19/util/RISWeekArchiveActivity.java
@@ -0,0 +1,16 @@
+package com.w2njl.VillanovaCovid19.util;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.w2njl.VillanovaCovid19.R;
+
+public class RISWeekArchiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_r_i_s_week_archive);
+    }
+}
